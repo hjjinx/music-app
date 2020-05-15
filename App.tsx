bc2363026c050f8e9be755fbae67a197ebc58a22
Himanshu Jain <hjmanojain.8@gmail.com>
2020-05-15 18:59:02 +0530

    add TabNavigation: Home and Library tabs

--- App.tsx
+++ App.tsx
@@ -1,19 +1,77 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from "react-navigation-tabs";
+import Icon from "react-native-vector-icons/Ionicons";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Home</Text>
+      </View>
+    );
+  }
 }
+
+class LibraryScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Library</Text>
+      </View>
+    );
+  }
+}
+
+const MainTabNavigator = createBottomTabNavigator(
+  {
+    Home: HomeScreen,
+    Library: LibraryScreen,
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Icon;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = focused ? "ios-home" : "ios-home";
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+        } else if (routeName === "Library") {
+          iconName = focused ? "ios-list-box" : "ios-list";
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: "green",
+      inactiveTintColor: "gray",
+      showLabel: false,
+      style: {
+        backgroundColor: "black",
+        borderBottomLeftRadius: 1,
+        borderColor: "green",
+        paddingBottom: 10,
+      },
+    },
+  }
+);
+
+export default createAppContainer(MainTabNavigator);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#222",
+    color: "green",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "green",
   },
 });
